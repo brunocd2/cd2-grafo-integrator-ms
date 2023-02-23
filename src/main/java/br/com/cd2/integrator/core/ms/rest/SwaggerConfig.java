bada65bec57ac4cb92c66c93ceb5de82d7632efc
brunocd2 <124630337+brunocd2@users.noreply.github.com>
2023-02-22 21:12:36 -0300

    Update SwaggerConfig.java

--- src/main/java/br/com/cd2/integrator/core/ms/rest/SwaggerConfig.java
+++ src/main/java/br/com/cd2/integrator/core/ms/rest/SwaggerConfig.java
@@ -7,9 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -24,7 +27,7 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.any())
-				.build()
+				.build().apiInfo(apiInfo())
 				.useDefaultResponseMessages(false)
 				.globalResponseMessage(RequestMethod.GET
 						,responseMessageGET());
@@ -45,6 +48,17 @@ public class SwaggerConfig {
 					.build()
 					);
 		}};
+	}
+	
+	private ApiInfo apiInfo() {
+	    return new ApiInfoBuilder()
+	            .title("CD2 Grafo Integrator ms")
+	            .description("Sistema responsável integração e insersão dos produtos de clientes internos e externos gerados no projeto CD2 Grafo Integrator.")
+	            .version("1.0.0")
+	            .license("CD2 licenced 2.0")
+	            .licenseUrl("https://www.cd2.com.br")
+	            .contact(new Contact("Bruno Santos", "https://www.cd2.com.br", "bruno.santos@cd2.com.br"))
+	            .build();
 	}
 	
 }
