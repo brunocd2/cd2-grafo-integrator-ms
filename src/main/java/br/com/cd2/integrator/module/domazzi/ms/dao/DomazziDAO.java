18c7c959118c3d6be948de9fe7fc99823857d455
brunocd2 <124630337+brunocd2@users.noreply.github.com>
2023-02-22 23:07:17 -0300

    Update DomazziDAO.java

--- src/main/java/br/com/cd2/integrator/module/domazzi/ms/dao/DomazziDAO.java
+++ src/main/java/br/com/cd2/integrator/module/domazzi/ms/dao/DomazziDAO.java
@@ -1,5 +1,9 @@
 package br.com.cd2.integrator.module.domazzi.ms.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +20,49 @@ public class DomazziDAO {
 	
 	public Boolean saveProducts(String sql) {
 		
-		Boolean novoProduto = false;
 		try {
-			return novoProduto = repository.insertProductProcessed(sql);
-		} catch (Exception e) {
+
+			Class.forName("org.postgresql.Driver");
+			
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? "
+					+ "Include in your library path!");
 			e.printStackTrace();
 			return false;
+
 		}
-		
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://ec2-54-173-77-184.compute-1.amazonaws.com:5432/dab7dm6f9nlecn", "zydcmkrqlequyr",
+					"7be726068f9523dc3a22d0eb11c9b35f283a101baa7e7d726750eb5e4cf5f896");
+			 
+			
+			
+			PreparedStatement pstm = connection.prepareStatement(sql);
+			
+			pstm.execute();
+			pstm.close();
+                        System.out.println("produto adicionado ");
+                        connection.close();
+                        return true;
+
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return false;
+
+		}
+
 	}
+		
+	
 	
 	public List<Domazzi> findAll() {
 		List<Domazzi> novoProduto = null;
