75b227bc42205e317017aca195550491429e9700
brunocd2 <124630337+brunocd2@users.noreply.github.com>
2023-02-22 21:02:31 -0300

    Update CD2Integrator.java

--- src/main/java/br/com/cd2/integrator/core/ms/CD2Integrator.java
+++ src/main/java/br/com/cd2/integrator/core/ms/CD2Integrator.java
@@ -2,8 +2,16 @@ package br.com.cd2.integrator.core.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Configuration
+@EntityScan(basePackages = { "br.com.cd2.integrator" })
+@EnableJpaRepositories(basePackages = { "br.com.cd2.integrator" })
+@ComponentScan(basePackages = {"br.com.cd2.integrator"})
 public class CD2Integrator {
 
 	public static void main(String[] args) {
