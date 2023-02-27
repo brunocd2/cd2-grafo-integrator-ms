61d2fd0e4369ef5bab47cb06beb70281fd59fbd4
brunocd2 <124630337+brunocd2@users.noreply.github.com>
2023-02-27 16:12:18 -0300

    Update DomazziDAO.java

--- src/main/java/br/com/cd2/integrator/module/domazzi/ms/dao/DomazziDAO.java
+++ src/main/java/br/com/cd2/integrator/module/domazzi/ms/dao/DomazziDAO.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.cd2.integrator.module.domazzi.ms.dto.DomazziDTO;
 import br.com.cd2.integrator.module.domazzi.ms.model.Domazzi;
 import br.com.cd2.integrator.module.domazzi.ms.repository.DomazziRepositoy;
 
@@ -64,47 +66,186 @@ public class DomazziDAO {
 		
 	
 	
-	public List<Domazzi> findAll() {
-		List<Domazzi> novoProduto = null;
+	public List<DomazziDTO> findAll() {
+		List<DomazziDTO> novoProduto = new ArrayList<>();
+		List<Domazzi> domazzi = repository.findAll();
+		DomazziDTO dto = new DomazziDTO();
 		try {
-			return novoProduto = repository.findAll();
+		for (int i = 0; i < domazzi.size(); i++) {
+			dto.setCategoria(domazzi.get(i).getCategoria());
+			dto.setCodigo_acesso_principal(domazzi.get(i).getCodigo_acesso_principal());
+			dto.setCreated_at(domazzi.get(i).getCreated_at());
+			dto.setCusto_ult_compra_cd(domazzi.get(i).getCusto_ult_compra_cd());
+			dto.setData_ult_compra(domazzi.get(i).getData_ult_compra());
+			dto.setDescricao_domazzi(domazzi.get(i).getDescricao_domazzi());
+			dto.setDescricao_produto(domazzi.get(i).getDescricao_produto());
+			dto.setDta_ult_saida(domazzi.get(i).getDta_ult_saida());
+			dto.setEstoque_12meses(domazzi.get(i).getEstoque_12meses());
+			dto.setEstoque_3meses(domazzi.get(i).getEstoque_3meses());
+			dto.setEstq_disponivel(domazzi.get(i).getEstq_disponivel());
+			dto.setExportador(domazzi.get(i).getExportador());
+			dto.setId(domazzi.get(i).getId());
+			dto.setId_produto(domazzi.get(i).getId_produto());
+			dto.setParceiro(domazzi.get(i).getParceiro());
+			dto.setPreco_vda_alvim(domazzi.get(i).getPreco_vda_alvim());
+			dto.setQtd_ult_saida(domazzi.get(i).getQtd_ult_saida());
+			dto.setReferencia_domazzi(domazzi.get(i).getReferencia_domazzi());
+			dto.setUpdated_at(domazzi.get(i).getUpdated_at());
+			
+			novoProduto.add(dto);
+		}
+		
+			return novoProduto;
 		} catch (Exception e) {
 			e.printStackTrace();
-			return novoProduto;
+			return null;
 		}
 		
 	}
 	
-	public Domazzi findById(Long id) {
-		Domazzi novoProduto = null;
+	public DomazziDTO findById(Long id) {
+		Domazzi novoProduto = repository.findId(id);;
+		
+		DomazziDTO dto = new DomazziDTO();
 		try {
-			return novoProduto = repository.findId(id);
+			dto.setCategoria(novoProduto.getCategoria());
+			dto.setCodigo_acesso_principal(novoProduto.getCodigo_acesso_principal());
+			dto.setCreated_at(novoProduto.getCreated_at());
+			dto.setCusto_ult_compra_cd(novoProduto.getCusto_ult_compra_cd());
+			dto.setData_ult_compra(novoProduto.getData_ult_compra());
+			dto.setDescricao_domazzi(novoProduto.getDescricao_domazzi());
+			dto.setDescricao_produto(novoProduto.getDescricao_produto());
+			dto.setDta_ult_saida(novoProduto.getDta_ult_saida());
+			dto.setEstoque_12meses(novoProduto.getEstoque_12meses());
+			dto.setEstoque_3meses(novoProduto.getEstoque_3meses());
+			dto.setEstq_disponivel(novoProduto.getEstq_disponivel());
+			dto.setExportador(novoProduto.getExportador());
+			dto.setId(novoProduto.getId());
+			dto.setId_produto(novoProduto.getId_produto());
+			dto.setParceiro(novoProduto.getParceiro());
+			dto.setPreco_vda_alvim(novoProduto.getPreco_vda_alvim());
+			dto.setQtd_ult_saida(novoProduto.getQtd_ult_saida());
+			dto.setReferencia_domazzi(novoProduto.getReferencia_domazzi());
+			dto.setUpdated_at(novoProduto.getUpdated_at());
+			
+			return dto;
 		} catch (Exception e) {
 			e.printStackTrace();
-			return novoProduto;
+			return null;
 		}
 		
 	}
 	
-	public List<Domazzi> findByName(String name) {
-		List<Domazzi> novoProduto = null;
+	public List<DomazziDTO> findByName(String name) {
+		List<DomazziDTO> novoProduto = new ArrayList<>();
+		List<Domazzi> domazzi = repository.findByName(name);
+		DomazziDTO dto = new DomazziDTO();
 		try {
-			return novoProduto = repository.findByName(name);
+		for (int i = 0; i < domazzi.size(); i++) {
+			dto.setCategoria(domazzi.get(i).getCategoria());
+			dto.setCodigo_acesso_principal(domazzi.get(i).getCodigo_acesso_principal());
+			dto.setCreated_at(domazzi.get(i).getCreated_at());
+			dto.setCusto_ult_compra_cd(domazzi.get(i).getCusto_ult_compra_cd());
+			dto.setData_ult_compra(domazzi.get(i).getData_ult_compra());
+			dto.setDescricao_domazzi(domazzi.get(i).getDescricao_domazzi());
+			dto.setDescricao_produto(domazzi.get(i).getDescricao_produto());
+			dto.setDta_ult_saida(domazzi.get(i).getDta_ult_saida());
+			dto.setEstoque_12meses(domazzi.get(i).getEstoque_12meses());
+			dto.setEstoque_3meses(domazzi.get(i).getEstoque_3meses());
+			dto.setEstq_disponivel(domazzi.get(i).getEstq_disponivel());
+			dto.setExportador(domazzi.get(i).getExportador());
+			dto.setId(domazzi.get(i).getId());
+			dto.setId_produto(domazzi.get(i).getId_produto());
+			dto.setParceiro(domazzi.get(i).getParceiro());
+			dto.setPreco_vda_alvim(domazzi.get(i).getPreco_vda_alvim());
+			dto.setQtd_ult_saida(domazzi.get(i).getQtd_ult_saida());
+			dto.setReferencia_domazzi(domazzi.get(i).getReferencia_domazzi());
+			dto.setUpdated_at(domazzi.get(i).getUpdated_at());
+			
+			novoProduto.add(dto);
+		}
+		
+			return novoProduto;
 		} catch (Exception e) {
 			e.printStackTrace();
-			return novoProduto;
+			return null;
 		}
 		
 	}
 	
-	public List<Domazzi> findByEanAndPartner(Long ean,String partner) {
-		List<Domazzi> novoProduto = null;
+	public List<DomazziDTO> findByEanAndPartner(Long ean,String partner) {
+		List<DomazziDTO> novoProduto = new ArrayList<>();
+		List<Domazzi> domazzi = repository.findByEanAndPartner(ean,partner);
+		DomazziDTO dto = new DomazziDTO();
 		try {
-			return novoProduto = repository.findByEanAndPartner(ean,partner);
+		for (int i = 0; i < domazzi.size(); i++) {
+			dto.setCategoria(domazzi.get(i).getCategoria());
+			dto.setCodigo_acesso_principal(domazzi.get(i).getCodigo_acesso_principal());
+			dto.setCreated_at(domazzi.get(i).getCreated_at());
+			dto.setCusto_ult_compra_cd(domazzi.get(i).getCusto_ult_compra_cd());
+			dto.setData_ult_compra(domazzi.get(i).getData_ult_compra());
+			dto.setDescricao_domazzi(domazzi.get(i).getDescricao_domazzi());
+			dto.setDescricao_produto(domazzi.get(i).getDescricao_produto());
+			dto.setDta_ult_saida(domazzi.get(i).getDta_ult_saida());
+			dto.setEstoque_12meses(domazzi.get(i).getEstoque_12meses());
+			dto.setEstoque_3meses(domazzi.get(i).getEstoque_3meses());
+			dto.setEstq_disponivel(domazzi.get(i).getEstq_disponivel());
+			dto.setExportador(domazzi.get(i).getExportador());
+			dto.setId(domazzi.get(i).getId());
+			dto.setId_produto(domazzi.get(i).getId_produto());
+			dto.setParceiro(domazzi.get(i).getParceiro());
+			dto.setPreco_vda_alvim(domazzi.get(i).getPreco_vda_alvim());
+			dto.setQtd_ult_saida(domazzi.get(i).getQtd_ult_saida());
+			dto.setReferencia_domazzi(domazzi.get(i).getReferencia_domazzi());
+			dto.setUpdated_at(domazzi.get(i).getUpdated_at());
+			
+			novoProduto.add(dto);
+		}
+		
+			return novoProduto;
 		} catch (Exception e) {
 			e.printStackTrace();
-			return novoProduto;
+			return null;
 		}
+		
+		
+	}
+	
+	public List<DomazziDTO> findByCategoria(String categoria) {
+		List<DomazziDTO> novoProduto = new ArrayList<>();
+		List<Domazzi> domazzi = repository.findByCategoria(categoria);
+		DomazziDTO dto = new DomazziDTO();
+		try {
+		for (int i = 0; i < domazzi.size(); i++) {
+			dto.setCategoria(domazzi.get(i).getCategoria());
+			dto.setCodigo_acesso_principal(domazzi.get(i).getCodigo_acesso_principal());
+			dto.setCreated_at(domazzi.get(i).getCreated_at());
+			dto.setCusto_ult_compra_cd(domazzi.get(i).getCusto_ult_compra_cd());
+			dto.setData_ult_compra(domazzi.get(i).getData_ult_compra());
+			dto.setDescricao_domazzi(domazzi.get(i).getDescricao_domazzi());
+			dto.setDescricao_produto(domazzi.get(i).getDescricao_produto());
+			dto.setDta_ult_saida(domazzi.get(i).getDta_ult_saida());
+			dto.setEstoque_12meses(domazzi.get(i).getEstoque_12meses());
+			dto.setEstoque_3meses(domazzi.get(i).getEstoque_3meses());
+			dto.setEstq_disponivel(domazzi.get(i).getEstq_disponivel());
+			dto.setExportador(domazzi.get(i).getExportador());
+			dto.setId(domazzi.get(i).getId());
+			dto.setId_produto(domazzi.get(i).getId_produto());
+			dto.setParceiro(domazzi.get(i).getParceiro());
+			dto.setPreco_vda_alvim(domazzi.get(i).getPreco_vda_alvim());
+			dto.setQtd_ult_saida(domazzi.get(i).getQtd_ult_saida());
+			dto.setReferencia_domazzi(domazzi.get(i).getReferencia_domazzi());
+			dto.setUpdated_at(domazzi.get(i).getUpdated_at());
+			
+			novoProduto.add(dto);
+		}
+		
+			return novoProduto;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 		
 	}
 	
